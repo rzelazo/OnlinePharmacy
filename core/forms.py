d904ed1af329b2a94c71a5a17913fccc1d360615
AleksandraSidor <101874595+AleksandraSidor@users.noreply.github.com>
2022-06-15 21:24:43 +0200

    update

--- core/forms.py
+++ core/forms.py
@@ -4,4 +4,4 @@ from .models import CartItem
 
 
 class AddItemToCartForm(forms.Form):
-    n_pieces = forms.IntegerField(min_value=1, max_value=99, label="Number of pieces")
+    n_pieces = forms.IntegerField(min_value=1, max_value=99)
